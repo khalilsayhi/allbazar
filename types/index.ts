ebb1a83fd1deb1e3eed3d39fd7f0a5c4d64b8fb2
khalil sayhi <khalil.sayhi01@gmail.com>
2025-05-25 16:32:39 +0200

    search logic + reviews

--- types/index.ts
+++ types/index.ts
@@ -5,6 +5,7 @@ import {
     insertOrderItemSchema,
     insertOrderSchema,
     insertProductSchema,
+    insertReviewSchema,
     paymentResultSchema,
     shippingAddressSchema
 } from "@/lib/validators";
@@ -12,6 +13,7 @@ import {
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
     rating: string;
+    numReviews: number;
     createdAt: Date;
 };
 
@@ -30,7 +32,11 @@ export type Order = z.infer<typeof insertOrderSchema> & {
     user: { name: string; email: string };
 };
 export type PaymentResult = z.infer<typeof paymentResultSchema>
-
+export type Review = z.infer<typeof insertReviewSchema> & {
+    id: string;
+    createdAt: Date;
+    user?: { name: string };
+};
 
 export const PAYMENT_METHODS = process.env.PAYMENT_METHODS ? process.env.PAYMENT_METHODS.split(",") : ["PayPal", "Stripe", "CashOnDelivery"];
 export const DEFAULT_PAYMENT_METHOD = "PayPal"
